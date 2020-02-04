826eaa7ad25c2687d6c3d0b2155dd0ad4d66effb
lucaisacoder <lucas_hsueh@hotmail.com>
2020-02-04 23:42:02 +0800

    update kconfig
    Change-Id: Icdb6f7bce91a984314b1a9bd24438bfb2a973647

--- util/Kconfig.cmake
+++ util/Kconfig.cmake
@@ -1,6 +1,18 @@
-set(gen_kconfig_cmd ${python}  ${SDK_SOURCE_DIR}/tools/kconfig/kconfig.py
-                        --kconfig "${CMAKE_SOURCE_DIR}/Kconfig"
-                        --menuconfig True
-                        )
+if(EXISTS ${PROJECT_SOURCE_DIR}/config_defaults.mk)
+    message(STATUS "Find project defaults config(config_defaults.mk)")
+    list(APPEND kconfig_defaults_files_args --defaults "${PROJECT_SOURCE_DIR}/config_defaults.mk")
+endif()
+if(EXISTS ${PROJECT_SOURCE_DIR}/.config.mk)
+    message(STATUS "Find project defaults config(config.mk)")
+    list(APPEND kconfig_defaults_files_args --defaults "${PROJECT_SOURCE_DIR}/.config.mk")
+endif()
 
+set(gen_kconfig_cmd ${python}  ${SDK_SOURCE_DIR}/tools/kconfig/kconfig.py
+        --kconfig "${CMAKE_SOURCE_DIR}/Kconfig"
+        ${kconfig_defaults_files_args}
+        --output makefile  ${PROJECT_BINARY_DIR}/config/global_config.mk
+        --output cmake  ${PROJECT_BINARY_DIR}/config/global_config.cmake
+        --output header ${PROJECT_BINARY_DIR}/config/global_config.h
+        --menuconfig True
+    )
 add_custom_target(menuconfig COMMAND ${gen_kconfig_cmd})
